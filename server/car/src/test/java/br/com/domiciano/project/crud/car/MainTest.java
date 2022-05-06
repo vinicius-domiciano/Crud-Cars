4aa3d412d0fff486d91266a512b9fd4c88bfe80b
Vinicius D. Alexandrino <viniciusalexdomiciano@gmail.com>
2022-05-05 23:39:44 -0300

    adding main test, to start test

---
+++ server/car/src/test/java/br/com/domiciano/project/crud/car/MainTest.java
@@ -0,0 +1,9 @@
+package br.com.domiciano.project.crud.car;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+@SpringBootTest
+public class MainTest {
+}
