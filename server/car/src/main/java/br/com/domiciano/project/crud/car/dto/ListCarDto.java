62b728f328e85b74e6c3cca59b8e4fc8f27db9fb
Vinicius D. Alexandrino <viniciusalexdomiciano@gmail.com>
2022-05-05 23:37:50 -0300

    Alter type fields

--- server/car/src/main/java/br/com/domiciano/project/crud/car/dto/ListCarDto.java
+++ server/car/src/main/java/br/com/domiciano/project/crud/car/dto/ListCarDto.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.util.Calendar;
 
 @Data
@@ -14,7 +15,7 @@ public class ListCarDto {
 
     private Long id;
     private String name;
-    private Double price;
+    private BigDecimal price;
     private String company;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
