d6c4447b6ba8bcbc680c864430479aa276a45783
Vinicius D. Alexandrino <viniciusalexdomiciano@gmail.com>
2022-05-05 23:35:53 -0300

    Added constructor

--- server/car/src/main/java/br/com/domiciano/project/crud/car/entity/Car.java
+++ server/car/src/main/java/br/com/domiciano/project/crud/car/entity/Car.java
@@ -5,6 +5,7 @@ import lombok.*;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Calendar;
 
 @Table(name = "car")
 @Entity
@@ -27,4 +28,11 @@ public class Car extends Base implements Serializable {
     @Column(name = "company", nullable = false)
     private String company;
 
+    public Car(Long id, Calendar dateCreated, Calendar dateUpdated, String name, Integer year, BigDecimal price, String company) {
+        super(id, dateCreated, dateUpdated);
+        this.name = name;
+        this.year = year;
+        this.price = price;
+        this.company = company;
+    }
 }
