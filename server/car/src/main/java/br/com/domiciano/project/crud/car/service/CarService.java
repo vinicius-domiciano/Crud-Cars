c5f82ac0b6f4b0a94d0a69f4c57e461b25d02850
Vinicius D. Alexandrino <viniciusalexdomiciano@gmail.com>
2022-05-05 23:37:30 -0300

    Rename class name

--- server/car/src/main/java/br/com/domiciano/project/crud/car/service/CarService.java
+++ server/car/src/main/java/br/com/domiciano/project/crud/car/service/CarService.java
@@ -2,7 +2,7 @@ package br.com.domiciano.project.crud.car.service;
 
 import br.com.domiciano.project.crud.car.dto.UpdateCarDto;
 import br.com.domiciano.project.crud.car.dto.FindCarDto;
-import br.com.domiciano.project.crud.car.dto.SaveCarDto;
+import br.com.domiciano.project.crud.car.dto.CreateCarDto;
 import br.com.domiciano.project.crud.car.dto.ListCarDto;
 
 import java.util.List;
@@ -11,7 +11,7 @@ public interface CarService {
 
     List<ListCarDto> listCars();
     FindCarDto findCarById(Long id);
-    SaveCarDto save(SaveCarDto carroDto);
+    CreateCarDto save(CreateCarDto carroDto);
     UpdateCarDto update(UpdateCarDto carroDto);
 
     void delete(Long id);
