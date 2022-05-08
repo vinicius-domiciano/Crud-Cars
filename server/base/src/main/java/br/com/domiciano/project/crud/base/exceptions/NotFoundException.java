247f4f678e75196258cc8683258bc71ae906d6d4
Vinicius D. Alexandrino <viniciusalexdomiciano@gmail.com>
2022-05-08 20:12:39 -0300

    rename param constructor

--- server/base/src/main/java/br/com/domiciano/project/crud/base/exceptions/NotFoundException.java
+++ server/base/src/main/java/br/com/domiciano/project/crud/base/exceptions/NotFoundException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 
 public class NotFoundException extends BaseRestException{
 
-    public NotFoundException(String mensagem) {
-        super(mensagem, HttpStatus.NOT_FOUND);
+    public NotFoundException(String message) {
+        super(message, HttpStatus.NOT_FOUND);
     }
 
 }
