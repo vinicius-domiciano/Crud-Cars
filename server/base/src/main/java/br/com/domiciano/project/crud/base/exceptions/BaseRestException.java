043597d3e356442a60f6eeb16d0895827e1da66c
Vinicius D. Alexandrino <viniciusalexdomiciano@gmail.com>
2022-05-08 20:15:40 -0300

    rename field

--- server/base/src/main/java/br/com/domiciano/project/crud/base/exceptions/BaseRestException.java
+++ server/base/src/main/java/br/com/domiciano/project/crud/base/exceptions/BaseRestException.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public class BaseRestException extends RuntimeException {
 
-    private String mensagem;
+    private String message;
     private HttpStatus httpStatus;
 
 }
