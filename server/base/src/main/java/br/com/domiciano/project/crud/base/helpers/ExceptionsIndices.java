1fb6ac950aeae8b019ee9a3d0b072986ea0bd61a
Vinicius D. Alexandrino <viniciusalexdomiciano@gmail.com>
2022-05-09 20:19:50 -0300

    Refactor sonar, creating private constructor

--- server/base/src/main/java/br/com/domiciano/project/crud/base/helpers/ExceptionsIndices.java
+++ server/base/src/main/java/br/com/domiciano/project/crud/base/helpers/ExceptionsIndices.java
@@ -5,5 +5,7 @@ public final class ExceptionsIndices {
     public static final String COMPANY_NOT_FOUND_ID_FORMAT = "Company not found to id: %s";
     public static final String CAR_NOT_FOUND_ID_FORMAT = "Car not found to id: %s";
 
+    ExceptionsIndices() {
+    }
 
 }
