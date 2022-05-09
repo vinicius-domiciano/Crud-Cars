d6df05e9f8735d1922e124072794a80f1c8e0b35
Vinicius D. Alexandrino <viniciusalexdomiciano@gmail.com>
2022-05-08 21:12:18 -0300

    implements new tests

--- server/car/src/test/java/br/com/domiciano/project/crud/car/service/CarServiceImplTest.java
+++ server/car/src/test/java/br/com/domiciano/project/crud/car/service/CarServiceImplTest.java
@@ -1,8 +1,11 @@
 package br.com.domiciano.project.crud.car.service;
 
+import br.com.domiciano.project.crud.base.exceptions.BaseRestException;
+import br.com.domiciano.project.crud.base.exceptions.NotFoundException;
 import br.com.domiciano.project.crud.car.annotation.CustomInitTest;
 import br.com.domiciano.project.crud.car.dto.CreateCarDto;
 import br.com.domiciano.project.crud.car.dto.ListCarDto;
+import br.com.domiciano.project.crud.car.dto.UpdateCarDto;
 import br.com.domiciano.project.crud.car.entity.Car;
 import br.com.domiciano.project.crud.car.repository.CarRepository;
 import br.com.domiciano.project.crud.car.service.impl.CarServiceImpl;
@@ -13,12 +16,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Optional;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.standaloneSetup;
 import static java.util.stream.Collectors.toList;
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -63,10 +69,61 @@ class CarServiceImplTest {
         assertTrue(this.getIds(carsDto).containsAll(this.getIds(resultReturn)));
     }
 
+    @Test
+    void haveToReturnEmptyList_inListCars() {
+        when(this.carRepository.findAll())
+                .thenReturn(new ArrayList<>());
+
+        var cars = this.carService.listCars();
+
+        assertTrue(cars.isEmpty());
+    }
+
     private List<Long> getIds(List<ListCarDto> carsDto) {
         return carsDto.stream()
                 .map(ListCarDto::getId)
                 .collect(toList());
+    }
+
+    @Test
+    void haveToReturnFindCarDto_inFindById() {
+        var now = Calendar.getInstance();
+
+        when(this.carRepository.findById(1L))
+                .thenReturn(Optional.of(new Car(1L, now, now, "HelloWorld", 2022, new BigDecimal("25.00"), "MyCompany")));
+
+        var response = new CreateCarDto(1L, "HelloWorld", "MyCompany", 2022, new BigDecimal("25.00"), now, now);
+        var carDto = this.carService.findCarById(1L);
+
+        assertEquals(response.getId(), carDto.getId());
+        assertEquals(response.getName(), carDto.getName());
+        assertEquals(response.getCompany(), carDto.getCompany());
+        assertEquals(response.getYear(), carDto.getYear());
+        assertTrue(response.getPrice().equals(carDto.getPrice()));
+        assertEquals(response.getDateCreated(), carDto.getDateCreated());
+        assertEquals(response.getDateUpdated(), carDto.getDateUpdated());
+    }
+
+    @Test
+    void haveToReturnThrowNotFoundCar_inFindCarById() {
+        when(this.carRepository.findById(1L))
+            .thenReturn(Optional.empty());
+
+        var isError = Boolean.FALSE;
+        var message = "";
+        var statusCode = 0;
+
+        try {
+            this.carService.findCarById(1L);
+        } catch(NotFoundException e) {
+            isError = Boolean.TRUE;
+            message = e.getMessage();
+            statusCode = e.getHttpStatus().value();
+        }
+
+        assertEquals(statusCode, 404);
+        assertEquals(message, "Car not found for id[1]");
+        assertTrue(isError);
     }
 
     @Test
@@ -91,6 +148,85 @@ class CarServiceImplTest {
         assertTrue(response.getPrice().equals(result.getPrice()));
         assertEquals(response.getDateCreated(), result.getDateCreated());
         assertEquals(response.getDateUpdated(), result.getDateUpdated());
+    }
+
+    @Test
+    void haveToUpdateAndReturnDtoObject_inUpdateCar() {
+        var car = new Car(1L, Calendar.getInstance(), Calendar.getInstance(), "HelloWorld", 2022, new BigDecimal("25.00"), "MyCompany");
+
+        when(this.carRepository.findById(1L))
+                .thenReturn(Optional.of(car));
+
+        var request = new UpdateCarDto(1L, "MyName", "MyCompany", 2022, new BigDecimal("25.00"), null);
+        var carUpdate = new Car(1L, null, null, "MyName", 2022, new BigDecimal("25.00"), "MyCompany");
+
+        when(this.carRepository.save(carUpdate))
+                .thenReturn(carUpdate);
+
+        var response = this.carService.update(request);
+
+        assertEquals(response.getId(), request.getId());
+        assertEquals(response.getName(), request.getName());
+        assertNotEquals(response.getName(), car.getName());
+        assertEquals(response.getCompany(), request.getCompany());
+        assertEquals(response.getYear(), request.getYear());
+        assertTrue(response.getPrice().equals(request.getPrice()));
+        assertNotEquals(response.getDataUpdated(), car.getDateUpdated());
+    }
+
+    @Test
+    void haveToReturnErrorNotFound_inUpdateCar() {
+        when(this.carRepository.findById(1L))
+                .thenReturn(Optional.empty());
+
+        var isError = Boolean.FALSE;
+        var message = "";
+        var statusCode = 0;
+
+        try {
+            var request = new UpdateCarDto(1L, "MyName", "MyCompany", 2022, new BigDecimal("25.00"), null);
+            this.carService.update(request);
+        } catch(NotFoundException e) {
+            isError = Boolean.TRUE;
+            message = e.getMessage();
+            statusCode = e.getHttpStatus().value();
+        }
+
+        assertEquals(statusCode, 404);
+        assertEquals(message, "Car not found for id[1]");
+        assertTrue(isError);
+    }
+
+    @Test
+    void haveToDeleteCar_inDeleteCar() {
+        var car = new Car(1L, Calendar.getInstance(), Calendar.getInstance(), "HelloWorld", 2022, new BigDecimal("25.00"), "MyCompany");
+
+        when(this.carRepository.findById(1L))
+                .thenReturn(Optional.of(car));
+
+        assertDoesNotThrow(() -> this.carService.delete(1L));
+    }
+
+    @Test
+    void haveToReturnNotFound_inDeleteCar() {
+        when(this.carRepository.findById(2L))
+                .thenReturn(Optional.empty());
+
+        var isError = Boolean.FALSE;
+        var message = "";
+        var statusCode = 0;
+
+        try {
+            this.carService.delete(2L);
+        } catch(NotFoundException e) {
+            isError = Boolean.TRUE;
+            message = e.getMessage();
+            statusCode = e.getHttpStatus().value();
+        }
+
+        assertEquals(statusCode, 404);
+        assertEquals(message, "Car not found for id[2]");
+        assertTrue(isError);
     }
 
 }
