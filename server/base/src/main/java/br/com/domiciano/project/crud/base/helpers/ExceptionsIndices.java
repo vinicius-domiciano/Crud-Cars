1eae61b2a4c5aac6cae9a876d5980b763575a2f7
Vinicius D. Alexandrino <viniciusalexdomiciano@gmail.com>
2022-05-09 20:18:39 -0300

    Refactor exceptions messages

---
+++ server/base/src/main/java/br/com/domiciano/project/crud/base/helpers/ExceptionsIndices.java
@@ -0,0 +1,9 @@
+package br.com.domiciano.project.crud.base.helpers;
+
+public final class ExceptionsIndices {
+
+    public static final String COMPANY_NOT_FOUND_ID_FORMAT = "Company not found to id: %s";
+    public static final String CAR_NOT_FOUND_ID_FORMAT = "Car not found to id: %s";
+
+
+}
