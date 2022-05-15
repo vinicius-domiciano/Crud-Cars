5ba58039cb01c05c61d9bc96cc534056a1635ff2
Vinicius D. Alexandrino <viniciusalexdomiciano@gmail.com>
2022-05-15 17:54:13 -0300

    Creating new route, update company

--- server/appServer/src/main/java/br/com/domiciano/project/crud/appserver/controller/CompanyController.java
+++ server/appServer/src/main/java/br/com/domiciano/project/crud/appserver/controller/CompanyController.java
@@ -3,6 +3,7 @@ package br.com.domiciano.project.crud.appserver.controller;
 import br.com.domiciano.project.crud.car.dto.FindCompanyDto;
 import br.com.domiciano.project.crud.car.dto.CreateCompanyDto;
 import br.com.domiciano.project.crud.car.dto.ListCompanyDto;
+import br.com.domiciano.project.crud.car.dto.UpdateCompanyDto;
 import br.com.domiciano.project.crud.car.service.CompanyCarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -30,11 +31,14 @@ public class CompanyController {
         return new ResponseEntity<>(companyCarService.findById(id), HttpStatus.OK);
     }
 
-    @PostMapping(value = "")
+    @PostMapping
     public ResponseEntity<CreateCompanyDto> saveCompany(@RequestBody @Valid CreateCompanyDto createCompanyDto) {
         return new ResponseEntity<>(companyCarService.create(createCompanyDto), HttpStatus.CREATED);
     }
 
-
+    @PutMapping
+    public ResponseEntity<UpdateCompanyDto> saveCompanyDto(@RequestBody @Valid UpdateCompanyDto updateCompanyDto) {
+        return new ResponseEntity<>(companyCarService.update(updateCompanyDto), HttpStatus.OK);
+    }
 
 }
