d2249c299c6a1c9cea1028690e295f894fd666b2
Vinicius D. Alexandrino <viniciusalexdomiciano@gmail.com>
2022-05-22 20:59:54 -0300

    Change methods.

--- server/car/src/main/java/br/com/domiciano/project/crud/car/service/impl/CarServiceImpl.java
+++ server/car/src/main/java/br/com/domiciano/project/crud/car/service/impl/CarServiceImpl.java
@@ -1,5 +1,6 @@
 package br.com.domiciano.project.crud.car.service.impl;
 
+import br.com.domiciano.project.crud.base.exceptions.BadRequestException;
 import br.com.domiciano.project.crud.base.helpers.ExceptionsIndices;
 import br.com.domiciano.project.crud.car.service.CarService;
 import br.com.domiciano.project.crud.base.exceptions.NotFoundException;
@@ -9,6 +10,7 @@ import br.com.domiciano.project.crud.car.dto.CreateCarDto;
 import br.com.domiciano.project.crud.car.dto.ListCarDto;
 import br.com.domiciano.project.crud.car.entity.Car;
 import br.com.domiciano.project.crud.car.repository.CarRepository;
+import br.com.domiciano.project.crud.car.service.CompanyCarService;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,23 +18,26 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class CarServiceImpl implements CarService {
 
-    private final CarRepository repository;
+    private final CarRepository carRepository;
+    private final CompanyCarService companyCarService;
 
     private static final ModelMapper MAPPER = new ModelMapper();
 
     @Autowired
-    public CarServiceImpl(CarRepository repository) {
-        this.repository = repository;
+    public CarServiceImpl(CarRepository carRepository, CompanyCarService companyCarService) {
+        this.carRepository = carRepository;
+        this.companyCarService = companyCarService;
     }
 
     @Override
     public List<ListCarDto> listCars() {
         return MAPPER.map(
-                repository.findAll(),
+                carRepository.findAll(),
                 new TypeToken<List<ListCarDto>>() {}.getType()
         );
     }
@@ -40,7 +45,7 @@ public class CarServiceImpl implements CarService {
     @Override
     public FindCarDto findCarById(Long id) {
         return MAPPER.map(
-                this.repository
+                this.carRepository
                         .findById(id)
                         .orElseThrow(() -> new NotFoundException(ExceptionsIndices.CAR_NOT_FOUND_ID_FORMAT, id)),
                 FindCarDto.class
@@ -50,8 +55,14 @@ public class CarServiceImpl implements CarService {
     @Override
     @Transactional(rollbackFor = { Exception.class })
     public CreateCarDto save(CreateCarDto createCarDto) {
+        if (Objects.isNull(createCarDto.getCompany()) || Objects.isNull(createCarDto.getCompany().getId())) {
+            throw new BadRequestException("Is need to set company id.");
+        }
+
+        this.companyCarService.findById(createCarDto.getCompany().getId());
+
         return MAPPER.map(
-                this.repository.save(MAPPER.map(createCarDto, Car.class)),
+                this.carRepository.save(MAPPER.map(createCarDto, Car.class)),
                 CreateCarDto.class
         );
     }
@@ -59,10 +70,15 @@ public class CarServiceImpl implements CarService {
     @Override
     @Transactional(rollbackFor = { Exception.class })
     public UpdateCarDto update(UpdateCarDto updateCarDto) {
+        if (Objects.isNull(updateCarDto.getCompany()) || Objects.isNull(updateCarDto.getCompany().getId())) {
+            throw new BadRequestException("Is need to set company id.");
+        }
+
+        this.companyCarService.findById(updateCarDto.getCompany().getId());
         this.findCarById(updateCarDto.getId());
 
         return MAPPER.map(
-                this.repository.save(MAPPER.map(updateCarDto, Car.class)),
+                this.carRepository.save(MAPPER.map(updateCarDto, Car.class)),
                 UpdateCarDto.class
         );
     }
@@ -71,6 +87,6 @@ public class CarServiceImpl implements CarService {
     public void delete(Long id) {
         this.findCarById(id);
 
-        this.repository.deleteById(id);
+        this.carRepository.deleteById(id);
     }
 }
