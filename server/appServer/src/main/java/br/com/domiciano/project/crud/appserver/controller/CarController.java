60c21c5bbbca55ea4f2aeadbfc637f069e9286ef
vinicius-domiciano <viniciusalexdomiciano@gmail.com>
2024-01-02 19:09:03 -0300

    add POST and Delete in car controller

--- server/appServer/src/main/java/br/com/domiciano/project/crud/appserver/controller/CarController.java
+++ server/appServer/src/main/java/br/com/domiciano/project/crud/appserver/controller/CarController.java
@@ -1,8 +1,6 @@
 package br.com.domiciano.project.crud.appserver.controller;
 
-import br.com.domiciano.project.crud.car.dto.UpdateCarDto;
-import br.com.domiciano.project.crud.car.dto.FindCarDto;
-import br.com.domiciano.project.crud.car.dto.ListCarDto;
+import br.com.domiciano.project.crud.car.dto.*;
 import br.com.domiciano.project.crud.car.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -29,9 +27,20 @@ public class CarController {
         return new ResponseEntity<>(this.carService.findCarById(id), HttpStatus.OK);
     }
 
+    @PostMapping
+    public ResponseEntity<SavedCarDto> saveCar(@Valid @RequestBody SaveCarDto saveCarDto) {
+        return new ResponseEntity<>(carService.save(saveCarDto), HttpStatus.CREATED);
+    }
+
     @PutMapping
     public ResponseEntity<UpdateCarDto> update(@Valid @RequestBody UpdateCarDto updateCarDto) {
         return new ResponseEntity<>(this.carService.update(updateCarDto), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/id")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable(name = "id") Long id) {
+        this.carService.delete(id);
     }
 
 }
